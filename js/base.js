bd394900a844522db11fe42e7c32dc65df74961c
Sean Mooney <smooney914@gmail.com>
2016-07-15 12:03:09 -0400

    Added smooth scrolling

---
+++ js/base.js
@@ -0,0 +1,31 @@
+jQuery(document).ready(function($) {
+    $("#about-scroll").click(function(event){     
+        $('html,body').animate({
+        	scrollTop:$("#about").offset().top
+        }, 500);
+    });
+});
+
+jQuery(document).ready(function($) {
+    $("#skills-scroll").click(function(event){     
+        $('html,body').animate({
+        	scrollTop:$("#about").offset().top
+        }, 500);
+    });
+});
+
+jQuery(document).ready(function($) {
+    $("#resume-scroll").click(function(event){     
+        $('html,body').animate({
+        	scrollTop:$("#about").offset().top
+        }, 500);
+    });
+});
+
+jQuery(document).ready(function($) {
+    $("#contact-scroll").click(function(event){     
+        $('html,body').animate({
+        	scrollTop:$("#about").offset().top
+        }, 500);
+    });
+});
